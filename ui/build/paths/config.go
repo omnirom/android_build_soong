30f944703da7fb367f3c30eadfaff10e13996735
Ali Hasan <ahb7671@gmail.com>
2020-10-03 10:56:20 +0500

    build: soong: whitelist nproc and perl
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>
    Change-Id: I556237240391668e5be5f78e950efa8e50498b4a

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -94,6 +94,8 @@ var Configuration = map[string]PathConfig{
 	"tr":      Allowed,
 	"unzip":   Allowed,
 	"zip":     Allowed,
+	"nproc":   Allowed,
+	"perl":    Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
