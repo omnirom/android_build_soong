6b3ec75d39335e92c1236ea68564a55d6eb7a46c
Inseob Kim <inseob@google.com>
2024-08-08 17:47:14 +0900

    Build product/odm build.prop with Soong
    Bug: 322090587
    Test: compare build.prop before and after, boot
    Change-Id: I7a7dc785030a0c17e5c09b7f65c978fe9bbcb7a7

--- android/variable.go
+++ android/variable.go
@@ -520,6 +520,7 @@ type ProductVariables struct {
 
 	SystemPropFiles    []string `json:",omitempty"`
 	SystemExtPropFiles []string `json:",omitempty"`
+	ProductPropFiles   []string `json:",omitempty"`
 
 	EnableUffdGc *string `json:",omitempty"`
 }
