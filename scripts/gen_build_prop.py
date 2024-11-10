148d64be3e3210611e1111e919a2650fec4407b0
micky387 <mickaelsaibi@free.fr>
2024-11-09 21:03:38 -0500

    build release if we say so in soong
    from https://github.com/omnirom/android_build/commit/a4762e67d34d8fde59f839ffda95807f10fa297e
    
    Change-Id: I0d03f528513323d30905c768f637f3343a81f60e

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -41,9 +41,12 @@ def get_build_flavor(product_config):
 
 def get_build_keys(product_config):
   default_cert = product_config.get("DefaultAppCertificate", "")
-  if default_cert == "" or default_cert == os.path.join(TEST_KEY_DIR, "testKey"):
+  if "ROM_BUILDTYPE" in product_config:
+    return "release-keys"
+  elif default_cert == "":
+    return "dev-keys"
+  elif default_cert == os.path.join(TEST_KEY_DIR, "testKey"):
     return "test-keys"
-  return "dev-keys"
 
 def override_config(config):
   if "PRODUCT_BUILD_PROP_OVERRIDES" in config:
